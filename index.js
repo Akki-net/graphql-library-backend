d0d7dbcd58833f5b16b2584ca4838a49434e659e
unknown <akash@cstech.in>
2022-03-19 20:28:04 +0530

    the query of allBook works for both optional paramenter altogether

--- index.js
+++ index.js
@@ -106,15 +106,18 @@ const resolvers = {
         authorCount: () => authors.length,
         bookCount: () => books.length,
         allBooks: (root, args) => {
-            if(args.author){
+
+            if(args.author && args.genre){
+                return books.filter(b => b.author === args.author && b.genres.includes(args.genre))
+            }
+            else if(args.author){
                 return books.filter(b => b.author === args.author)
-            }
-
-            if(args.genre){
+            }else if(args.genre){
                 return books.filter(b => b.genres.includes(args.genre))
+            }else{
+                return books
             }
 
-            return books
         },
         allAuthors: () => authors
     },
